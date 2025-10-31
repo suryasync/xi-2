009ec74c4bbd3a74c6163aad15b562b81f27132e
Surya Wiranto <suryawiranto15@gmail.com>
2025-10-31 21:03:50 +0700

    Fix bug

--- balinosa/scripts/footer.js
+++ balinosa/scripts/footer.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("/balinosa/footer.html")
+  fetch("balinosa/footer.html")
     .then((res) => {
       if (!res.ok) throw new Error("Footer not found");
       return res.text();
